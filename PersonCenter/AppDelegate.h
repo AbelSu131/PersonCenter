5979300f72b38ea9a217908ef790c3463739b857
AbelSu <abel@abeldemacbook-pro.local>
2015-09-17 18:00:01 +0800

    Initial Commit

---
+++ PersonCenter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PersonCenter
+//
+//  Created by abel on 15/9/17.
+//  Copyright © 2015年 abel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PersonCenter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PersonCenter
+//
+//  Created by abel on 15/9/17.
+//  Copyright © 2015年 abel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
